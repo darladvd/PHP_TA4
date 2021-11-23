eaebad0d9006438d8480d61334d1e9ebf94e329c
Darla David <201910063@feualabang.edu.ph>
2021-11-24 01:00:20 +0800

    Deleted files.

--- admin_home.php
+++ admin_home.php
@@ -6,7 +6,7 @@
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Admin Home</title>  
+    <title>Admin Panel</title>  
     <style> 
     h1, p
     {
@@ -17,39 +17,32 @@
     table
     {
         font-family: Montserrat, sans-serif;      
-        height: 800px;
-        width: 1000px;
+        /* height: 500px; */
+        /* width: 800px; */
         border: 5px solid #1F4172;
         border-radius: 4px;
+    }
+
+    img
+    {
+        height: 200px;
     }
     </style>  
 </head>
 
 <body>
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>"" enctype="multipart/form-data">
     <?php
-        session_start();
-        error_reporting(0);
+    session_start();
+        // if (!isset($_SESSION['username']) || !isset($_SESSION['password']))
+        // {
+		// 	header("Location: login.php");
+		// }
+		// elseif ($_SESSION['userlevel'] == "user")
+        // {
+		// 	header("Location: user_home.php");
+		// }
 
-        if (!isset($_SESSION['username']) || !isset($_SESSION['password']))
-        {
-			header("Location: login.php");
-		}
-		elseif ($_SESSION['userlevel'] == "user")
-        {
-			header("Location: user_home.php");
-		}
-
-        $firsname = $_SESSION['firsname'];
-        $middlename = $_SESSION['middlename'];
-        $lastname = $_SESSION['lastname'];
-        $contact = $_SESSION['contact'];
-        $email - $_SESSION['email'];
-        $birthday = $_SESSION['birthday'];
         $username = $_SESSION['username'];
-        $password = $_SESSION['password'];
-        $accesslvl - $_SESSION['accesslvl'];
-        $status = $_SESSION['status'];
 
         $conn = mysqli_connect('localhost','root','','ta4');
 
@@ -59,63 +52,22 @@
             die("Connection Failed: ". $conn->connect_error);
         } else 
         {
-            $sql = "SELECT * FROM info WHERE username = '$username' AND password = '$password'";
+            $sql = "SELECT * FROM info WHERE username = '$username'";
             $exec = mysqli_query($conn, $sql);
 
             while($row = mysqli_fetch_array($exec))
             {
-                echo "<table align='center' cellpadding = '10'>";
-                echo "<th><h1>Admin Account</h1></th>";
-                echo "<tr><td><p><a href='login.php'>Log-out</a></p></td><td><p><a href='info-viewusers.php'>View Records</a></p></td><td><p><a href='info-admin_app.php'>Add Record</a></p></td></tr>";
-                echo "<tr><td><p><b>Welcome</b><br>".$row['username']."(".$row['userlevel'].")<br></p></td>";
-                echo "<td><img src=images/".$row['image']."></td></tr>";
-                echo "<tr><td><p><b>Userlevel:</b><br>".$row['userlevel']."<br></p></td>";
-                echo "<td><input type='file' name='image'></td><td><input type = 'submit' name = 'save' value = 'save changes'></td></form>";
-                    if (isset($_POST['save']) && isset($_FILES['image']))
-                    {
-                        $imgName = $_FILES['image']['name'];
-                        $imgSize = $_FILES['image']['size'];
-                        $tmpName = $_FILES['image']['tmp_name'];
-                        $error = $_FILES['image']['error'];
-
-                        if ($error === 0){
-                            if ($imgSize > 5000000)
-                            {
-                                echo "Sorry, your file size is too large.";
-                            } else
-                            {
-                                $imgEx = pathinfo($imgName, PATHINFO_EXTENSION);
-                                $imgExLc = strtolower($imgEx);
-                                
-                                $allowed_exs = array("jpg", "jpeg", "png");
-                                
-                                if (in_array($imgExLc, $allowed_exs))
-                                {
-                                    $new_imgName = uniqid("IMG-", true).'.'.$imgExLc;
-                                    $img_upload_path = 'images/'.$new_imgName;
-                                    move_uploaded_file($tmpName, $img_upload_path);
-                                    
-                                    $exec2 = "UPDATE info SET image = '" . $new_imgName . "' WHERE username = '".$username."'"; 
-                                    mysqli_query($conn, $exec2);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            echo "Error.";
-                        }
-                    }
-                echo "</td></tr>";
-                echo "<tr><td><p><b>Email:</b><br>".$row['email']."<br><hr></p></td></tr>";
-                echo "<th><h2>Reset Password</h2></th>";
-                echo "<tr><td><p><b>Enter Current Password: <input type='password' name='cpassword'><br></b></p></td></tr>";
-                echo "<tr><td><p><b>Enter New Password: <input type='password' name='npassword'><br></b></p></td></tr>";
-                echo "<tr><td><p><b>Re-Enter New Password: <input type='password' name='n2password'><br></b></p></td></tr>";
-                echo "<tr><td><p><input type='submit'></td></tr>";
-                echo "</table>"; 
+                echo "<table align='center' cellpadding = '5'>";
+                echo "<tr><td><h1>My Information</h1></td>";
+                echo "<td><p><a href='login.php'>Log-out</a></p></td></tr>";
+                echo "<tr><td><p><b>Welcome</b> ".$row['firstname']." ".$row['middlename']." ".$row['lastname']."</p></td></tr>";
+                echo "<tr><td><p><b>Userlevel: </b> ".$row['accesslvl']."</p></td></tr>";
+                echo "<tr><td><p><b>Birthday: </b> ".$row['birthday']."</p></td></tr>";
+                echo "<tr><td><p><b>Contact Details</b></p></td></tr>";
+                echo "<tr><td><p><b>&nbsp;&nbsp;Contact: </b> ".$row['contact']."</p></td></tr>";
+                echo "<tr><td><p><b>&nbsp;&nbsp;Email: </b> ".$row['email']."</p></td></tr>";
             }
         }
     ?>
-    </form>
 </body>
 </html>
--- user_home.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "mukhang hatdog";
-?>
